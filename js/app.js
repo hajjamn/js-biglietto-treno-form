ea98238a6e462cc02ccff7b7821edefa43f7867d
hajjamn <hajjamn@gmail.com>
2024-03-05 14:25:09 +0100

    ADD: added listener to button

--- js/app.js
+++ js/app.js
@@ -10,19 +10,31 @@ console.log('Età = ' + etaElement)
 const kilometraggioElement = document.getElementById("kilometraggio")
 console.log('Kilometraggio = ' + kilometraggioElement)
 
+//Creo costante e ascolto il pulsante invia
+
+const inviaElement = document.querySelector('button');
+console.log(`Il pulsante di invio è ${inviaElement}`)
+
+
+inviaElement.addEventListener('click',
+function() {
+  console.log(`Il pulsante &{inviaElement} è stato cliccato`)
+}
+);
+  
 //Calcolo Prezzo Base Biglietto facendo Kilometri x 0.21
 
-let prezzoBase = kilometraggio * 0.21;
+let prezzoBase = kilometraggioElement.value * 0.21;
 console.log('Prezzo base = ' + prezzoBase)
-
+  
 //Calcolo Sconto per Minorenne o Oltre i 65 anni, altrimenti lo fisso per 0%
 
 let sconto
 
-if (eta <= 18) {
+if (etaElement.value <= 18) {
   sconto = prezzoBase * 0.2;
   console.log('Sconto = ' + sconto)
-} else if (eta >= 65) {
+} else if (etaElement.value >= 65) {
   sconto = prezzoBase * 0.4;
   console.log('Sconto = ' + sconto)
 } else {
